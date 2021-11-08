e32f105eca00d200648ad8349a7223410f09b637
Didier Vera <didier.mono@hotmail.com>
2021-11-08 11:35:38 -0500

    first commit

---
+++ Helpers/mainApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace mainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("format numer app: ");
+
+            var value = double.Parse(Console.ReadLine());
+
+            MoneyHelperCL.MoneyFormatHelper helper = new MoneyHelperCL.MoneyFormatHelper();
+            var valueFormater = helper.FormatNumber(value);
+            Console.WriteLine(string.Format("previus value formetter is: {0}", valueFormater));
+            
+        }
+    }
+}
