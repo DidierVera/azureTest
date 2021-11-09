c5bb1860fcd0926f7d4bf2f432d1cbebdaa72abf
Didier Vera <didier.mono@hotmail.com>
2021-11-09 08:17:34 -0500

    order files in directory, remove helper folder

---
+++ MoneyHelperCL/MoneyFormatHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MoneyHelperCL
+{
+    public class MoneyFormatHelper
+    {
+        /// <summary>
+        /// Set numeric separator and decimal valid
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="number"></param>
+        /// <param name="maxDecimals"></param>
+        /// <returns></returns>
+        public string FormatNumber<T>(T number, int maxDecimals = 4)
+        {
+            return Regex.Replace(String.Format("{0:n" + maxDecimals + "}", number),
+                                 @"[" + System.Globalization.NumberFormatInfo.CurrentInfo.NumberDecimalSeparator + "]?0+$", "");
+        }
+    }
+}
